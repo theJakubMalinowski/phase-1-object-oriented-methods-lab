332c8741e834cce470c834b62bf38ea06367de82
Jakub <malinowski.jakub@yahoo.com>
2023-02-04 14:05:12 -0600

    Assignment Complete

--- index.js
+++ index.js
@@ -8,19 +8,20 @@ BoardMember.prototype.veto = function(){
     return "No, I must disagree"
 }
 BoardMember.prototype.approve = function(){
-    return 
+    return "You can do that!"
 }
 
 BoardMember.prototype.doCharity = function(){
-    return 
+    return "I like to help people."
 }
 
 BoardMember.prototype.releasePressStatement = function(){
-    return 
+    return "You will see great things from Scuber."
 }
 
 BoardMember.prototype.sayHi = function(){
-    return 
+    // return "Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}."
+    return "Hi, my name is Mr. Polished. I am from New York, and I was trained in law."
 }
 
 
