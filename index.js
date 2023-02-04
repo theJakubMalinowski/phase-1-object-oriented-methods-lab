4155efba6b6aebba5f26d5ef7c1117b0f168cd2a
Jakub <malinowski.jakub@yahoo.com>
2023-02-04 13:56:44 -0600

    functions called

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function BoardMember(name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+}
+BoardMember.prototype.veto = function(){
+    return "No, I must disagree"
+}
+BoardMember.prototype.approve = function(){
+    return 
+}
+
+BoardMember.prototype.doCharity = function(){
+    return 
+}
+
+BoardMember.prototype.releasePressStatement = function(){
+    return 
+}
+
+BoardMember.prototype.sayHi = function(){
+    return 
+}
+
+
